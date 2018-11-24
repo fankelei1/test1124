6cd5f8d1a641703448ad923a18bb1bb3638a5207
8230459 <8230459@qq.com>
2018-11-24 15:39:48 +0800

    修改README

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,55 @@
 <template lang="jade">
-  div(id="app")
-    div(id="nav")
+  -var c = 'app'
+  -var wrap = c + '__wrap'
+
+  div(class="#{wrap}")
+    //div(id="nav")
       router-link(to="/") Home |
       router-link(to="/about") About
-    router-view
+    div(@click="buttonHandler") button
+    drawer(:show.sync="isShowDrawer", showMode="push")
+      div(slot="drawer")
+        div(class="aaa") 123
+      div
+        router-view
 </template>
+<script>
+  import {Drawer} from 'vux'
 
-<style lang="postcss">
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
-  #nav {
-    padding: 30px;
-    a {
-      font-weight: bold;
-      color: #2c3e50;
-      &.router-link-exact-active {
-        color: #42b983;
+  export default {
+    components: {
+      Drawer
+    },
+    data() {
+      return {
+        isShowDrawer: false
+      }
+    },
+    methods: {
+      buttonHandler() {
+        this.isShowDrawer = !this.isShowDrawer
+        console.log(this.isShowDrawer)
       }
     }
+  }
+</script>
+<style>
+  @c app {
+    @d wrap {
+      width: 100%;
+      min-height: 300px;
+      overflow-x: hidden;
+    }
+  }
+  .vux-drawer {
+
+  }
+  .vux-drawer > .vux-drawer-content {
+    width: 200px;
+    height: 400px !important;
+    background: orange !important;
+  }
+  .vux-drawer > .vux-drawer-body > .drawer-mask {
+    height: 400px !important;
   }
 </style>
